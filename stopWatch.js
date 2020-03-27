ff3093b94d13768b34c6a74a55c29e6282daf9f9
Shova-Acharya <shova.723@gmail.com>
2020-03-27 17:48:59 +0545

    Fix minor issues

--- stopWatch.js
+++ stopWatch.js
@@ -1,59 +1,51 @@
 let time = document.querySelector(".timer");
+
 let second = 0;
 let minute = 0;
 let hour = 0;
-let startCountdown = 0;
- 
+let timerInterval;
 
+function renderTime(){
+    time.textContent = 
+    (hour<10? "0"+hour : hour) + ":" +
+    (minute<10? "0"+minute: minute) + ":" +
+    (second<10? "0"+second:second) ;   
+}
+ 
 function onStart(){
-    startCountdown = setInterval(countdown, 1000);
+    if(timerInterval == null){
+        timerInterval = setInterval(countdown, 1000);
+    }  
 }
 
 function countdown(){
-    time.textContent = `${hour}:${minute}:${second}`
-
+    renderTime();
     second++;
 
     //adding the minute
     if (second > 59){
         minute ++;
         second = 0;
-        second++;
     }
     //adding the hour
     if(minute > 59){
         hour++;
         minute = 0;
-        minute++;
     }
-
-   time.textContent = 
-    (hour<10? "0"+hour : hour) + ":" +
-    (minute<10? "0"+minute: minute) + ":" +
-    (second<10? "0"+second:second) ;
-
 }
 
 function onPause(){
-    clearInterval(startCountdown);
-    startCountdown = 0;  
+    clearInterval(timerInterval);
+    timerInterval = undefined;  
 }
 
 function onStop(){
-    clearInterval(startCountdown);
-    time.textContent="00:00:00"
-    second = 0;
-    minute = 0;
-    hour = 0;
+    clearInterval(timerInterval);
+    timerInterval = undefined;
+    renderTime(second = 0, minute = 0, hour = 0);
 }
 
-
-
 function onRestart(){
-    clearInterval(startCountdown);
-    time.textContent="00:00:00"
-    second = 0;
-    minute = 0;
-    hour = 0;
-    onStart();
+   onStop();
+   onStart();
 }
